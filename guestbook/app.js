2e8ca8fb21fc608ee8d6b2f77a59ba2907dbc3c0
sriee <psg.sriee@gmail.com>
2019-05-24 19:05:25 -0700

    app with two endpoints

---
+++ guestbook/app.js
@@ -0,0 +1,53 @@
+var http = require("http");
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+
+// Make the entries local to all views
+var entries = []
+app.locals.entries = entries
+
+// Set EJS as our view rendering engine 
+app.set("views", path.resolve(__dirname, "views"));	// Path to view directory
+app.set("view engine", "ejs");
+
+// Logging middleware
+app.use(logger("dev"));
+
+// POST body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.get("/new-entry", (req, res) => {
+	res.render("new-entry");
+});
+
+app.post("/new-entry", (req, res) => {
+	if(!res.body.title || !res.body.body) {
+		res.status(400).send("Entries must have body and title");
+		return;
+	} else {
+		entries.push({
+			title: req.body.title,
+			content: req.body.body,
+			published: new Date()
+		});
+	}
+	res.redirect("/");
+});
+
+app.use((req, res) = {
+	res.status(404).render("404");
+});
+
+http.createServer(app).listen(3000, () => {
+	console.log("Guest Book server has started at port 3000.");
+});
