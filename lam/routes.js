a3f5a9ffb4908784d143713459c233b58f28f867
sriee <psg.sriee@gmail.com>
2019-05-26 14:51:56 -0700

    handle api routes

---
+++ lam/routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const User = require("./models/users");
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	res.locals.errors = req.flash("error");
+	res.locals.infos = req.flash("info");
+	next();
+});
+
+
+router.get("/", (req, res, next) => {
+	User.find()
+	.sort({ createdAt: "decending" })
+	.exec((err, users) => {
+		if(err) {
+			return next(err)
+		}
+		res.render("index", { users: users });
+	});
+});
+
+export { router as routes };
