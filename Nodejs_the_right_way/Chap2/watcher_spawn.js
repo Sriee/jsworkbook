370efdd825e44556e5060eaff2b2ca0156c8227a
sriee <psg.sriee@gmail.com>
2020-04-27 06:32:34 -0700

    spawn process

---
+++ Nodejs_the_right_way/Chap2/watcher_spawn.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const fs = require("fs");
+const spawn = require("child_process").spawn;
+const filename = process.argv[2];
+
+if (!filename)
+  throw Error("Missing argument 'filename'. Usage: node watcher.sj <filename>");
+
+fs.watch(filename, () => {
+  const ls = spawn("ls", ["-l", "-h", filename]);
+  ls.stdout.pipe(process.stdout);
+});
+
+console.log(`Started watching ${filename} for changes!`);
