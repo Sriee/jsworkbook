4b696ad43c44a3014fc34dffe22d85aec1db7e43
sriee <psg.sriee@gmail.com>
2020-05-08 16:47:25 -0700

    add GET request handler

--- Nodejs_the_right_way/Chap6/index.js
+++ Nodejs_the_right_way/Chap6/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const program = require("commander");
+const request = require("request");
 const pkg = require("./package.json");
 
 program
@@ -9,7 +10,7 @@ program
   .usage("[options] <commands> [...]")
   .option("-o, --hostname <hostname>", "Hostname [localhost]", "localhost")
   .option("-p, --port <number>", "Port no [9200]", "9200")
-  .option("-j, --json", "Format output as JSON")
+  .option("-j, --json", "Format output as JSON", false)
   .option("-i, --index <name>", "Which index to use")
   .option("-t, --type <type>", "Default type for bulk operation");
 
@@ -27,4 +28,24 @@ program
   .command("url [path]")
   .description("generate the URL for option and path [default = '/']")
   .action((path = "/") => console.log(fullURL(path)));
+
+program
+  .command("get [path]")
+  .description("get request to Elastic search cluster [default='/']")
+  .action((path = "/") => {
+    let url = fullURL(path);
+    console.log(url + " " + program.json);
+    let options = {
+      url: url,
+      json: program.json,
+    };
+    request.get(options, (err, res, body) => {
+      if (program.json) {
+        console.log(JSON.stringify(err || body));
+      } else {
+        if (err) throw err;
+        console.log(body);
+      }
+    });
+  });
 program.parse(process.argv);
